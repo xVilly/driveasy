f05a04a85a318c04a54690c56b7f8520f03e178e
xVilly <mich038al@gmail.com>
2023-12-12 19:17:16 +0100

    configure project for javafx, maven deps and plugins

--- src/main/java/com/driveasy/App.java
+++ src/main/java/com/driveasy/App.java
@@ -3,7 +3,24 @@ package com.driveasy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("Hello World!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         Connection connection = null;
         try {
@@ -17,5 +34,6 @@ public class App {
         } catch (Exception e) {
             System.out.println("Error: "+e.getMessage());
         }
+        launch();
     }
 }
